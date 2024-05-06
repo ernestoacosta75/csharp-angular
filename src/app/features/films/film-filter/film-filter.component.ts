9f1823cccf93c4e29353f3bc5343e1207d862fb8
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2024-05-06 16:18:31 +0200

    Fims search component logic

--- src/app/features/films/film-filter/film-filter.component.ts
+++ src/app/features/films/film-filter/film-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import * as R from 'ramda';
 
 @Component({
   selector: 'app-film-filter',
@@ -9,21 +10,62 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class FilmFilterComponent implements OnInit {
   
   form: FormGroup;
+
+  originalForm = {
+    title: '',
+    genderId: 0,
+    nextReleases: false,
+    onCinemas: false
+  };
+
   genders = [
     { id: 1, name: 'Drama' },
     { id: 2, name: 'Comedy' },
-    { id: 3, name: 'Thirller' },
+    { id: 3, name: 'Thriller' },
+    { id: 4, name: 'Action' },
+    { id: 5, name: 'Fantasy' },
   ];
+
+  films = [
+    { title: 'The Shawshank Redemption', onCinemas: false, nextReleases: true, genders: [1, 3], poster: 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX100_CR0,1,100,148_.jpg' },
+    { title: 'The Godfather', onCinemas: false, nextReleases: true, genders: [1], poster: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UY148_CR2,0,100,148_.jpg' },
+    { title: 'The Godfather: Part II', onCinemas: true, nextReleases: false, genders: [1], poster: 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UY148_CR2,0,100,148_.jpg' },
+    { title: 'The Dark Knight', onCinemas: true, nextReleases: false, genders: [3, 4], poster: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_QL75_UX100_CR0,0,100,148_.jpg' },
+    { title: '12 Angry Men', onCinemas: false, nextReleases: true, genders: [1, 2], poster: 'https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_QL75_UX100_CR0,3,100,148_.jpg' },
+    { title: 'Schindler\'s List', onCinemas: false, nextReleases: true, genders: [1, 3], poster: 'https://m.media-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL75_UX100_CR0,1,100,148_.jpg' },
+    { title: 'Pulp Fiction', onCinemas: true, nextReleases: false, genders: [3], poster: 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UY148_CR1,0,100,148_.jpg' },
+    { title: 'The Lord of the Rings: The Return of the King', onCinemas: true, nextReleases: false, genders: [4, 5], poster: 'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX100_CR0,0,100,148_.jpg' },
+  ];
+
+  originalFilms = R.clone(this.films);
 
   constructor(private formBuilder: FormBuilder) {}
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      title: '',
-      genderId: 0,
-      nextReleases: false,
-      onCinemas: false
+    this.form = this.formBuilder.group(this.originalForm);
+
+    this.form.valueChanges.subscribe(values => {
+      this.films = R.clone(this.originalFilms);
+      this.searchFilms(values);
     });
   }
 
-  onClick = () => this.form.reset();
+  searchFilms = (values: any) => {
+    if (R.isNotNil(R.path(['title'], values))) {
+      this.films = R.filter(film => film.title.toLowerCase().includes(values.title.toLowerCase()), this.films);
+    }
+
+    if (R.not(R.pathEq(0, ['genderId'], values))) {
+      this.films = R.filter(film => R.includes(values.genderId, film.genders), this.films);
+    }
+
+    if (R.pathEq(true, ['nextReleases'], values)) {
+      this.films = R.filter(film => film.nextReleases, this.films);
+    }
+
+    if (R.pathEq(true, ['onCinemas'], values)) {
+      this.films = R.filter(film => film.onCinemas, this.films);
+    }
+  }
+
+  onClick = () => this.form.patchValue(this.originalForm);
 }
