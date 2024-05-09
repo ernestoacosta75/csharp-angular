38673e5480b604286f02cf7081c9ae6b336afe32
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2024-05-09 12:33:41 +0200

    Actors

---
+++ src/app/features/actors/new-actor/new-actor.component-HQAPEW1C002-BCB.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-actor',
+  templateUrl: './new-actor.component.html',
+  styleUrl: './new-actor.component.css'
+})
+export class NewActorComponent {
+
+}
---
+++ src/app/genders/edit-gender/edit-gender.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GenderDto } from '../../features/genders/models/gender';
+
+@Component({
+  selector: 'app-edit-gender',
+  templateUrl: './edit-gender.component.html',
+  styleUrl: './edit-gender.component.css'
+})
+export class EditGenderComponent {
+
+  model: GenderDto = {
+    name: 'Drama'
+  };
+
+  constructor(private router: Router) {
+    
+  }
+
+  onFormSubmitted = (genderDto: GenderDto) => {
+    // .. to save the changes
+    console.log(genderDto);
+    this.router.navigateByUrl('/genders');
+  }
+}
